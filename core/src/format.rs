e14519742793fc48fe9eab99c522687052055ffd
Ray Redondo <raydredondo@gmail.com>
2022-11-08 01:00:57 -0600

    Read track header, fix minor details

--- core/src/format.rs
+++ core/src/format.rs
@@ -11,7 +11,7 @@ pub struct InstrumentTrack {}
 
 #[derive(Debug)]
 pub enum TrackBody {
-    Instrument(InstrumentTrack),
+    Instrument(Option<InstrumentTrack>),
     Pattern, // Beat & Bassline. TODO: Deprecate and convert to something else
     Sample(),
     Unused3, // Formerly EventTrack.
@@ -109,12 +109,12 @@ impl ProjectFile {
         }
     }
 
-    fn load_head(e: &BytesStart) -> io::Result<ProjectHead> {
+    fn load_head(e: &BytesStart, is_empty: bool) -> io::Result<ProjectHead> {
         let mut head = ProjectHead::default();
-        for attrib in e.attributes() {
-            let Ok(attrib) = attrib else { return Err(io::Error::new(io::ErrorKind::InvalidData, "bad attribute")) };
-            let value = attrib.unescape_value().map_err(|x| invalid_data!(x))?;
-            match attrib.key.as_ref() {
+        for attr in e.attributes() {
+            let Ok(attr) = attr else { Err(invalid_data!("bad attribute"))? };
+            let value = attr.unescape_value().map_err(|x| invalid_data!(x))?;
+            match attr.key.as_ref() {
                 b"bpm" => {
                     head.bpm = f32::from_str(&value).map_err(|x| invalid_data!(x))?;
                 }
@@ -133,30 +133,64 @@ impl ProjectFile {
                 x => todo!("{}", std::str::from_utf8(x).unwrap()),
             }
         }
+        if !is_empty {
+            todo!(); // What does this mean here?
+        }
         Ok(head)
     }
 
     #[allow(clippy::needless_pass_by_value)] // TODO: Re-evaluate
-    fn load_track(reader: &mut Reader<impl BufRead + Seek>, _e: BytesStart) -> io::Result<Track> {
+    fn load_track(
+        reader: &mut Reader<impl BufRead + Seek>,
+        e: BytesStart,
+        is_empty: bool,
+    ) -> io::Result<Track> {
         let mut buf = Vec::new();
 
-        let track = Track {
+        let mut track = Track {
             name: String::new(),
             solo: false,
             mute: false,
-            body: TrackBody::Unused3, // Temp
+            body: TrackBody::Instrument(None), // Temp
         };
-        loop {
-            match reader.read_event_into(&mut buf) {
-                Ok(Event::End(e)) => match e.name().as_ref() {
-                    b"track" => break,
-                    _ => Err(invalid_data!("unexpected end tag"))?,
-                },
-                Ok(Event::Text(e)) => assert_whitespace!(e),
-                Ok(Event::Eof) => unexpected_eof!(),
-                Ok(x) => todo!("{:?}", x),
-                Err(Error::Io(x)) => return Err(x),
-                Err(x) => return Err(io::Error::new(io::ErrorKind::InvalidData, x)),
+
+        for attr in e.attributes() {
+            let Ok(attr) = attr else { Err(invalid_data!("bad attribute"))? };
+            let value = attr.unescape_value().map_err(|x| invalid_data!(x))?;
+            match attr.key.as_ref() {
+                b"name" => track.name = value.into(),
+                b"solo" => track.solo = value == "1",
+                b"muted" => track.mute = value == "1",
+                b"type" => {
+                    track.body = match value.as_ref() {
+                        "0" => TrackBody::Instrument(None),
+                        "1" => TrackBody::Pattern,
+                        "2" => TrackBody::Sample(),
+                        "3" => TrackBody::Unused3,
+                        "4" => TrackBody::Unused4,
+                        "5" => TrackBody::Automation(),
+                        "6" => TrackBody::HiddenAutomation(),
+                        x => todo!("{x}"),
+                    }
+                }
+                b"mutedBeforeSolo" => {} // GUI element
+                x => todo!("{}", std::str::from_utf8(x).unwrap()),
+            }
+        }
+
+        if !is_empty {
+            loop {
+                match reader.read_event_into(&mut buf) {
+                    Ok(Event::End(e)) => match e.name().as_ref() {
+                        b"track" => break,
+                        _ => Err(invalid_data!("unexpected end tag"))?,
+                    },
+                    Ok(Event::Text(e)) => assert_whitespace!(e),
+                    Ok(Event::Eof) => unexpected_eof!(),
+                    Ok(x) => todo!("{:?}", x),
+                    Err(Error::Io(x)) => return Err(x),
+                    Err(x) => return Err(io::Error::new(io::ErrorKind::InvalidData, x)),
+                }
             }
         }
         Ok(track)
@@ -177,7 +211,7 @@ impl ProjectFile {
                         match reader.read_event_into(&mut buf) {
                             Ok(Event::Empty(e)) => match e.name().as_ref() {
                                 b"head" => {
-                                    result.head = Self::load_head(&e)?;
+                                    result.head = Self::load_head(&e, true)?;
                                 }
                                 x => todo!("{}", std::str::from_utf8(x).unwrap()),
                             },
@@ -190,7 +224,11 @@ impl ProjectFile {
                                                     Ok(Event::Start(e)) => {
                                                         match e.name().as_ref() {
                                                             b"track" => result.tracks.push(
-                                                                Self::load_track(&mut reader, e)?,
+                                                                Self::load_track(
+                                                                    &mut reader,
+                                                                    e,
+                                                                    false,
+                                                                )?,
                                                             ),
                                                             x => todo!(
                                                                 "{}",
