8a7a632735a87246f69f476354cfcdcb7a201a92
Ray Redondo <raydredondo@gmail.com>
2022-11-09 18:35:45 -0600

    Fix compilation error

--- core/src/format.rs
+++ core/src/format.rs
@@ -92,6 +92,7 @@ macro_rules! unexpected_eof {
 fn convert<T>(str: &str) -> io::Result<T>
 where
     T: FromStr + Default,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static
 {
     T::from_str(str)
         .map_err(|x| invalid_data!(x))
