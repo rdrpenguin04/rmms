14481a17faa0b1347a943d72eaa6db262499da55
Ray Redondo <raydredondo@gmail.com>
2022-11-10 03:23:21 -0600

    Start audio engine, add temporary FLAC output

--- core/src/audio.rs
+++ core/src/audio.rs
@@ -1,7 +1,96 @@
 #[cfg(feature = "cpal")]
 use cpal::{traits::HostTrait, StreamError};
 
-pub struct Engine;
+pub trait Sample {
+    fn from_sample(s: impl Sample) -> Self;
+    fn to_f32(self) -> f32;
+    fn to_i16(self) -> i16;
+    fn to_i32(self) -> i32;
+}
+
+impl Sample for f32 {
+    #[inline]
+    fn from_sample(s: impl Sample) -> Self {
+        s.to_f32()
+    }
+
+    #[inline]
+    fn to_f32(self) -> f32 {
+        self
+    }
+
+    #[inline]
+    fn to_i16(self) -> i16 {
+        (self * f32::from(i16::MAX)).round() as i16
+    }
+
+    #[inline]
+    fn to_i32(self) -> i32 {
+        (self * i32::MAX as f32).round() as i32
+    }
+}
+
+impl Sample for i16 {
+    #[inline]
+    fn from_sample(s: impl Sample) -> Self {
+        s.to_i16()
+    }
+
+    #[inline]
+    fn to_f32(self) -> f32 {
+        f32::from(self) / f32::from(i16::MAX)
+    }
+
+    #[inline]
+    fn to_i16(self) -> i16 {
+        self
+    }
+
+    #[inline]
+    fn to_i32(self) -> i32 {
+        self as i32 * i16::MAX as i32
+    }
+}
+
+impl Sample for i32 {
+    #[inline]
+    fn from_sample(s: impl Sample) -> Self {
+        s.to_i32()
+    }
+
+    #[inline]
+    fn to_f32(self) -> f32 {
+        self as f32 / i32::MAX as f32
+    }
+
+    #[inline]
+    fn to_i16(self) -> i16 {
+        (self / i16::MAX as i32) as i16
+    }
+
+    #[inline]
+    fn to_i32(self) -> i32 {
+        self
+    }
+}
+
+pub struct Engine {
+    pub play: bool,
+}
+
+impl Engine {
+    pub fn fill_buffer<T: Sample>(&mut self, buf: &mut [T]) {
+        for sample in buf {
+            *sample = Sample::from_sample(0);
+        }
+    }
+
+    pub fn new() -> Self {
+        Self {
+            play: false,
+        }
+    }
+}
 
 #[cfg(feature = "cpal")]
 #[derive(Debug)]
--- exporter/src/main.rs
+++ exporter/src/main.rs
@@ -1,10 +1,12 @@
 use std::{
     fs::File,
-    io::{self, BufReader},
+    io::{self, BufReader, Write},
 };
 
+use bitvec::{vec::BitVec, prelude::Msb0};
 use clap::Parser;
-use rmms_core::format::ProjectFile;
+use flacenc::component::BitRepr;
+use rmms_core::{format::ProjectFile, audio};
 
 /// Standalone Exporter for RMMS projects
 #[derive(Parser, Debug)]
@@ -12,8 +14,63 @@ struct Args {
     filename: String,
 }
 
+struct RenderOutput {
+    data: Box<[i32]>,
+    index: usize,
+}
+
+impl flacenc::source::Source for RenderOutput {
+    fn channels(&self) -> usize {
+        1
+    }
+
+    fn bits_per_sample(&self) -> usize {
+        32
+    }
+
+    fn sample_rate(&self) -> usize {
+        44100
+    }
+
+    fn read_samples(&mut self, dest: &mut flacenc::source::FrameBuf) -> Result<usize, flacenc::error::SourceError> {
+        let mut count = 0;
+        for x in dest.channel_slice_mut(0) {
+            if self.index >= self.data.len() {
+                break;
+            }
+            *x = self.data[self.index];
+            self.index += 1;
+            count += 1;
+        }
+        Ok(count)
+    }
+}
+
+impl RenderOutput {
+    fn new(data: Box<[i32]>) -> Self {
+        Self {
+            data,
+            index: 0,
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let args = Args::parse();
+
+    // ! TEMPORARY TEST CODE ! //
+    let mut audio_engine = audio::Engine::new();
+    let mut output = [0; 44100];
+    audio_engine.play = true;
+    audio_engine.fill_buffer(&mut output);
+    let render_output = RenderOutput::new(Box::from(output));
+    let encoder = flacenc::config::Encoder::default();
+    let stream = flacenc::coding::encode_with_fixed_block_size(&encoder, render_output, 2048).unwrap();
+    let mut bitvec: BitVec<u8, Msb0> = BitVec::with_capacity(stream.count_bits());
+    stream.write(&mut bitvec).unwrap();
+    File::create("output.flac").unwrap().write_all(bitvec.as_raw_slice()).unwrap();
+    // ! END TEST CODE ! //
+
     let file = ProjectFile::load(BufReader::new(File::open(args.filename)?))?;
     println!("{file:?}");
     Ok(())
